b5fe87c0728b7a4536e366a8ace8e531b99bd125
Adam Williams <awilliams@orbotix.com>
2014-04-02 21:16:45 -0600

    Added tests. Added settlement. Settlement increases population.

---
+++ Settlement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Game
+{
+	public class Settlement
+	{
+		public float PopulationProgress {
+			get;
+			private set;
+		}
+
+		public int Population {
+			get;
+			private set;
+		}
+
+		public Settlement ()
+		{
+		}
+
+		public void Turn ()
+		{
+			IncreasePopulationProgress ();
+		}
+
+		void IncreasePopulationProgress ()
+		{
+			PopulationProgress += 0.1f;
+			if (PopulationProgress >= 1f) {
+				PopulationProgress -= 1f;
+				Population++;
+			}
+		}
+	}
+}
+
---
+++ Tests/TestSettlement.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using Game;
+
+namespace Tests
+{
+	[TestFixture ()]
+	public class TestSettlement
+	{
+		Settlement m_settlement;
+
+		[SetUp]
+		public void Setup()
+		{
+			m_settlement = new Settlement ();
+		}
+
+		[Test ()]
+		public void TestPopulationProgressIncreasesWithTurn ()
+		{
+			var prev = m_settlement.PopulationProgress;
+			m_settlement.Turn ();
+
+			Assert.Greater (m_settlement.PopulationProgress, prev);
+		}
+
+
+		[Test ()]
+		public void TestPopulationIncreasesWhenProgressIsFull ()
+		{
+			TestPopulationProgressIncreasesWithTurn ();
+			Assert.Greater (m_settlement.PopulationProgress, 0f);
+			Assert.Less (m_settlement.PopulationProgress, 1f);
+
+			var prevPopulation = m_settlement.Population;
+			float prevProgress;
+			do {
+				prevProgress = m_settlement.PopulationProgress;
+				m_settlement.Turn();
+				Assert.Less(m_settlement.PopulationProgress, 1f); //never reaches 1
+			}while(m_settlement.PopulationProgress > prevProgress);
+
+			Assert.Greater (m_settlement.Population, prevPopulation);
+		}
+	}
+}
+
